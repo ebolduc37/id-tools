b981abe2f29c5dcd3bee6e3f498bb44c1b0ef3b0
Riviere Fougy <rivierefougy@gmail.com>
2023-03-07 21:48:16 -0500

    feat: example function and test, further configuration

---
+++ tests/example/example.test.js
@@ -0,0 +1,15 @@
+import example from "./example";
+
+describe("Jest Test Example", () => {
+  const expectedResult = 7;
+
+  it("returns the sum of two arguments passed into the example function", () => {
+    const a = 1;
+    const b = 1;
+    const expectedResult = 2;
+
+    const result = example(a, b);
+
+    expect(result).toEqual(expectedResult);
+  });
+});
