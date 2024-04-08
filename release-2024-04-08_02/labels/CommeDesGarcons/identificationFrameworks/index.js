e19e48132fea8c67a3da1cca23c0cd0d46ce512e
Etienne Bolduc <etienne.bolduc37@gmail.com>
2024-04-08 10:37:24 -0400

    Bug fix about logos

---
+++ release-2024-04-08_02/labels/CommeDesGarcons/identificationFrameworks/index.js
@@ -0,0 +1,16 @@
+/**
+ * COMME des GARÇONS identification frameworks index.
+ *
+ * Barrel file to assign the COMME des GARÇONS identification frameworks to InputCDG.
+ *
+ * @author Etienne Bolduc
+ */
+
+import "./Cdgcdg.js";
+import "./Forever.js";
+import "./ForeverException.js";
+import "./Monthly.js";
+import "./Seasonal.js";
+import "./SpecialCase.js";
+import "./Shirt.js";
+import "./ShirtException.js";
