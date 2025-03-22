163fa374c13e23a6f25ad722796598457b4dc699
Etienne Bolduc <etienne.bolduc37@gmail.com>
2025-03-21 21:58:58 -0400

    Update Yohji
    Updated the structure of the Yohji section and added new information

---
+++ release-2025-03-21_01/labels/YohjiYamamoto/identificationFrameworks/index.js
@@ -0,0 +1,9 @@
+/**
+ * Yohji Yamamoto identification frameworks index.
+ *
+ * Barrel file to assign the Yohji Yamamoto identification frameworks to InputYY.
+ *
+ * @author Etienne Bolduc
+ */
+
+import "./Regular.js";
