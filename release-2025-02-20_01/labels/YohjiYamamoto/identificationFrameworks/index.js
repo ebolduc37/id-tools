ec6fb727bdc80e9ec73ff20cc5e6b98a8ba1870e
Etienne Bolduc <etienne.bolduc37@gmail.com>
2025-04-02 21:15:01 -0400

    YY update
    major update in the identification of Y's, + NOIR, and more

--- release-2025-02-20_01/labels/YohjiYamamoto/identificationFrameworks/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Yohji Yamamoto identification frameworks index.
- *
- * Barrel file to assign the Yohji Yamamoto identification frameworks to InputYY.
- *
- * @author Etienne Bolduc
- */
-
-import "./Regular.js";
--- release-2025-03-22_02/labels/MaisonMargiela/identificationFrameworks/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Maison Margiela identification frameworks index.
- *
- * Barrel file to assign the Maison Margiela identification frameworks to InputMM.
- *
- * @author Etienne Bolduc
- */
-
-import "./Regular.js";
