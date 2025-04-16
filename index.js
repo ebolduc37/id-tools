80461a30877bb5c19ff53ffa1820721e2e04e25d
Etienne Bolduc <etienne.bolduc37@gmail.com>
2025-04-15 20:59:27 -0400

    Update module
    Update on the module used

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-import { identify, extract } from "./release-2025-04-15_01/index.js";
+import { identify, extract } from "./release-2025-04-15_02/index.js";
 
 // GET garment information
 app.get('/extract', async (req, res) => {
