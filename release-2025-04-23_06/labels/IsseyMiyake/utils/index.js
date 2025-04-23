3a23fc6f8dcaa352246f6d13f892d8089125f612
Etienne Bolduc <etienne.bolduc37@gmail.com>
2025-04-23 12:11:28 -0400

    Update do not tumble dry
    Update input confirmation of do not tumble dry

---
+++ release-2025-04-23_06/labels/IsseyMiyake/utils/index.js
@@ -0,0 +1,12 @@
+/**
+ * ISSEY MIYAKE utils index.
+ *
+ * Barrel file to export ISSEY MIYAKE classes, enums, and functions across the codebase.
+ *
+ * @author Etienne Bolduc
+ */
+
+// classes
+export { default as InputIM } from "./classes/InputIM.js"
+// enums
+// functions
