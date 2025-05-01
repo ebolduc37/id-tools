555244f484ac94efb130ae6f98cbde18b9272a5d
Etienne Bolduc <etienne.bolduc37@gmail.com>
2025-05-01 03:49:08 -0400

    Bug fix
    wrong check for Y's for men sans serif

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 const express = require("express");
-const id_function = require("../release-2025-05-01_01/index.js");
+const id_function = require("../release-2025-05-01_02/index.js");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
